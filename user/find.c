f57f385b621d1544df4578d5ba4e9e32d05654f8
Subhra S Sarkar <rurtle.coder@gmail.com>
2026-09-07 16:01:51 +0000

    Added basic plumbing taking inspiration from ls.c

--- user/find.c
+++ user/find.c
@@ -1,6 +1,81 @@
 #include "kernel/types.h"
 #include "kernel/stat.h"
 #include "user/user.h"
+#include "kernel/fs.h"
+#include "kernel/fcntl.h"
+
+
+char*
+fmtname(char *path)
+{
+  static char buf[DIRSIZ+1];
+  char *p;
+
+  // Find first character after last slash.
+  for(p=path+strlen(path); p >= path && *p != '/'; p--)
+    ;
+  p++;
+
+  // Return blank-padded name.
+  if(strlen(p) >= DIRSIZ)
+    return p;
+  memmove(buf, p, strlen(p));
+  memset(buf+strlen(p), ' ', DIRSIZ-strlen(p));
+  return buf;
+}
+
+void
+find(char *path, char *fn)
+{
+  char buf[512];
+  // char *p;
+  int fd;
+  // struct dirent de;
+  struct stat st;
+
+  if((fd = open(path, O_RDONLY)) < 0){
+    fprintf(2, "ls: cannot open %s\n", path);
+    return;
+  }
+
+  if(fstat(fd, &st) < 0){
+    fprintf(2, "ls: cannot stat %s\n", path);
+    close(fd);
+    return;
+  }
+
+  switch(st.type){
+  case T_DEVICE:
+  case T_FILE:
+    printf("%s %d %d %d\n", fmtname(path), st.type, st.ino, (int) st.size);
+    break;
+
+  case T_DIR:
+    if(strlen(path) + 1 + DIRSIZ + 1 > sizeof buf){
+      printf("ls: path too long\n");
+      break;
+    }
+    printf("%s %d %d %d\n", fmtname(path), st.type, st.ino, (int) st.size);
+#if 0
+    strcpy(buf, path);
+    p = buf+strlen(buf);
+    *p++ = '/';
+    while(read(fd, &de, sizeof(de)) == sizeof(de)){
+      if(de.inum == 0)
+        continue;
+      memmove(p, de.name, DIRSIZ);
+      p[DIRSIZ] = 0;
+      if(stat(buf, &st) < 0){
+        printf("ls: cannot stat %s\n", buf);
+        continue;
+      }
+      printf("%s %d %d %d\n", fmtname(buf), st.type, st.ino, (int) st.size);
+    }
+#endif
+    break;
+  }
+  close(fd);
+}
 
 int
 main(int argc, char *argv[])
@@ -10,6 +85,9 @@ main(int argc, char *argv[])
     fprintf(2, "Usage: find <path> <filename>\n");
     exit(1);
   }
+
+  // Parse the command line arguments
+  find(argv[1], argv[2]);
 
   // Graceful return
   exit(0);
